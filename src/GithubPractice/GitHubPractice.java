b028b8129e339f9d84bd61610d4b828757342bcd
rahimullahsiddiqi <rahimullah2009@gmail.com>
2023-05-31 09:18:26 -0400

    this is what i can do

--- src/GithubPractice/GitHubPractice.java
+++ src/GithubPractice/GitHubPractice.java
@@ -3,8 +3,12 @@ package GithubPractice;
 public class GitHubPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("I can do this ");
+		
+		
 
 	}
+	
 
 }
